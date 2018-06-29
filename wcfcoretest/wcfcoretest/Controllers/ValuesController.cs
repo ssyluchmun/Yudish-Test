f415a2b35ce5cb6ad5e2111abb1906342802acff
yudish.luchmun <yudish.luchmun@smarthealth.mu>
2018-06-29 13:43:22 +0400

    1

---
+++ wcfcoretest/wcfcoretest/Controllers/ValuesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wcfcoretest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
+        
+        // GET api/values
+        /*[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            yield return proxy.GetStudentsAsync().ToString();
+            //return new string[] { "value1", "value2" };
+
+
+        }*/
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //var x = await proxy.GetStudentsAsync();
+            ServiceReference1.Student newStudent =
+
+                new ServiceReference1.Student()
+
+                {
+
+                    StudentId = 0,
+
+                    FirstName = "cores",
+
+                    LastName = "cores",
+
+                    RegisterNo = "cores",
+
+                    Department = "cores"
+
+                };
+
+
+
+            await proxy.AddStudentsAsync(newStudent);
+
+            var x = await proxy.GetStudentsAsync();
+            return Ok(x);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        { 
+            return "value";
+        }
+
+        // POST api/values
+        //[HttpPost]
+        //public async Task<IActionResult> Put([FromBody]string value)
+        //{
+        //    var x = await proxy.GetStudentsAsync();
+
+        //    return Ok(x);
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Post(int id, [FromBody]string value)
+        //{
+        //    ServiceReference1.Student newStudent =
+
+        //        new ServiceReference1.Student()
+
+        //        {
+
+        //            StudentId = 5,
+
+        //            FirstName = "Mark",
+
+        //            LastName = "Dallas",
+
+        //            RegisterNo = "278589243579432",
+
+        //            Department = "DEPT"
+
+        //        };
+
+
+
+        //    await proxy.AddStudentsAsync(newStudent);
+
+        //    return null;
+        //}
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ wcfcoretest/wcfcoretest/RunnableTask.cs
@@ -0,0 +1,90 @@
+﻿using RecurrentTasks;
+using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Hl7.Fhir.Rest;
+using Hl7.Fhir.Model;
+using System.Diagnostics;
+using Hl7.Fhir.Serialization;
+
+namespace wcfcoretest
+{
+    public class RunnableTask : IRunnable
+    {
+        private ILogger logger;
+
+        public RunnableTask(ILogger<RunnableTask> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async System.Threading.Tasks.Task RunAsync(ITask currentTask, IServiceProvider scopeServiceProvider, CancellationToken cancellationToken)
+        {
+            //var URL = "http://hapi.fhir.org/baseDstu3";
+
+            /*
+            
+
+            var obs= new Observation();
+            obs.Status = ObservationStatus.Preliminary;
+            obs.Code = new CodeableConcept("http://example.org","my-example-code");
+
+            var result = client.Create<Observation>(obs);
+            */
+
+
+
+            //ServiceReference1.Service1Client proxy = new ServiceReference1.Service1Client();
+            //ServiceReference1.Student newStudent =
+
+            //   new ServiceReference1.Student()
+
+            //   {
+
+            //       StudentId = 0,
+
+            //       FirstName = "Mark",
+
+            //       LastName = "Dallas",
+
+            //       RegisterNo = "278589243579432",
+
+            //       Department = "DEPT"
+
+            //   };
+             
+
+           // await proxy.AddStudentsAsync(newStudent);
+            //hapi fhir starts here
+            //var client = new FhirClient("http://vonk.fire.ly");
+            //client.PreferredFormat = ResourceFormat.Json;
+            //Debug.WriteLine("LINE WRITE");
+            //var q = new SearchParams().LimitTo(10);
+            //Bundle result = client.Search<Claim>();
+
+            //FhirJsonSerializer sr = new FhirJsonSerializer();
+
+
+
+
+            //while (result != null)
+            //{
+            //    foreach (var e in result.Entry)
+            //    {
+            //        Claim p = (Claim)e.Resource;
+            //        var bundlexml = sr.SerializeToString(p);
+
+            //        Debug.WriteLine(bundlexml);
+            //    }
+
+            //    result = client.Continue(result, PageDirection.Next);
+            //}
+
+            //currentTask.Interval = currentTask.Interval.Add(TimeSpan.FromSeconds(20));
+            //await System.Threading.Tasks.Task.CompletedTask;
+
+            //ends here
+            
+        }
+    }
+}
